1678041cde26612c77d495e67c7a34deae88eb80
SnugSessionBox <123650565+SnugSessionBox@users.noreply.github.com>
2023-04-12 23:59:47 +0200

    Delete _app directory

--- _app/immutable/chunks/3.da3b80eb.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_page.2d508a7e.js";import{P as m}from"./_page.9837f9a3.js";export{m as component,e as universal};
--- _app/immutable/entry/host-session-_slug_-page.svelte.a6ad3e95.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/index.bad9930e.js";import"../chunks/container.5d3369bd.js";import{P as p}from"../chunks/_page.9837f9a3.js";export{p as default};
